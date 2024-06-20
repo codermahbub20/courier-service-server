a84762d6695d3b2840da1243f879ec012442d7a3
codermahbub20 <mkrefat5@gmail.com>
2024-06-21 00:26:04 +0600

    Users save in database

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -14,6 +16,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rarr4yf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -32,17 +35,83 @@ async function run() {
     // await client.connect();
 
     const packageCollection = client.db("niyamatDB").collection("package");
+    const usersCollection = client.db('niyamatDB').collection('users')
 
 
 
     // Post package in database
-    app.post('/package', async (req, res) => {
-       
+    app.post('/package', async (req, res) => {    
             const package = req.body;
             const result = await packageCollection.insertOne(package)
             res.send(result);
-
     });
+
+
+     // Save or modify user email, status in DB
+     app.put('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email: email }
+      const options = { upsert: true }
+      const isExist = await usersCollection.findOne(query)
+      console.log('User found?----->', isExist)
+      if (isExist) {
+        if (user?.status === 'Requested') {
+          const result = await usersCollection.updateOne(
+            query,
+            {
+              $set: user,
+            },
+            options
+          )
+          return res.send(result)
+        } else {
+          return res.send(isExist)
+        }
+      }
+      const result = await usersCollection.updateOne(
+        query,
+        {
+          $set: { ...user, timestamp: Date.now() },
+        },
+        options
+      )
+      res.send(result)
+    })
+
+
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log('I need a new jwt', user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
 
 
 
